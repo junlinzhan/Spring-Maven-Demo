f476c130ad4c14bbb004197f19857e36131179b7
jimin.zheng <jimin.zheng@qunar.com>
2016-03-20 22:43:23 +0800

    thread group

--- src/main/java/com/app/mvc/business/controller/TestController.java
+++ src/main/java/com/app/mvc/business/controller/TestController.java
@@ -59,4 +59,28 @@ public class TestController {
         return JsonData.success(redisPool.getResource().get(k));
     }
 
+    @ResponseBody
+    @RequestMapping(value = "thread.json")
+    public JsonData threadGroup(){
+        ThreadGroup group = Thread.currentThread().getThreadGroup();
+        ThreadGroup topGroup = group;
+        // 遍历线程组树，获取根线程组
+        while (group != null) {
+            topGroup = group;
+            group = group.getParent();
+        }
+        // 激活的线程数加倍
+        int estimatedSize = topGroup.activeCount() * 2;
+        Thread[] slackList = new Thread[estimatedSize];
+        // 获取根线程组的所有线程
+        int actualSize = topGroup.enumerate(slackList);
+        // copy into a list that is the exact size
+        Thread[] list = new Thread[actualSize];
+        System.arraycopy(slackList, 0, list, 0, actualSize);
+        log.info("Thread list size == " + list.length);
+        for (Thread thread : list) {
+            log.info(thread.getName());
+        }
+        return JsonData.success(list);
+    }
 }
