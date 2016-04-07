448ea21cb735361c24003da27fe4b4d93840f022
jimin.zheng <jimin.zheng@qunar.com>
2016-04-07 22:03:59 +0800

    add short url

---
+++ src/main/java/com/app/mvc/shortUrl/ShortUrlDao.java
@@ -0,0 +1,1 @@
+package com.app.mvc.shortUrl;import com.app.mvc.common.DBRepository;import org.apache.ibatis.annotations.Param;/** * Created by jimin on 16/4/7. */@DBRepositorypublic interface ShortUrlDao {    void save(ShortUrl shortUrl);    ShortUrl findByNewUrl(@Param("url") String url);}
