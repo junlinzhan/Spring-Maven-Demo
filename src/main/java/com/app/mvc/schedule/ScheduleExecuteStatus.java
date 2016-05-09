0228e7e1a6067574207fe883b690241374c1873e
jimin.zheng <jimin.zheng@qunar.com>
2016-05-09 12:09:51 +0800

    record schedule job execute information

---
+++ src/main/java/com/app/mvc/schedule/ScheduleExecuteStatus.java
@@ -0,0 +1,1 @@
+package com.app.mvc.schedule;import lombok.Getter;/** * Created by jimin on 16/5/9. */@Getterpublic enum ScheduleExecuteStatus {    RUNNING(0, "正在运行"),    FINISHED(1, "已经完成"),    EXCEPTION(2, "出现异常结束");    private int code;    private String desc;    ScheduleExecuteStatus(int code, String desc) {        this.code = code;        this.desc = desc;    }}
